a7f1a2d5be9e490f6b04f8ccbb318f5d51972216
DanielHadley <dphnrome@yahoo.com>
2014-11-26 16:21:47 -0500

    tgiving

--- MapSameSexMarriage.R
+++ MapSameSexMarriage.R
@@ -16,22 +16,10 @@ PolsByState <- read.csv("./data/PolsByState.csv")
 PollsByState <- read.csv("./data/PollsByState.csv")
 
 
-
 library(choroplethr)
 library(choroplethrMaps)
 library(ggplot2)
-
-
-# Opposed
-PolsByState$region <- tolower(PolsByState$State)
-PolsByState$value <- PolsByState$PercOfPolsOpp * 100
-
-state_choropleth(PolsByState,
-                 title   = "Politicians Who Oppose Gay Marriage",
-                 legend  = "% Opposed",
-                 buckets = 1)
-
-ggsave(paste("./plots/Map1.png"), dpi=300, width=7, height=4)
+library("gridExtra")
 
 
 # Politician Support
@@ -43,7 +31,7 @@ state_choropleth(PolsByState,
                  legend  = "% Support",
                  buckets = 1)
 
-ggsave(paste("./plots/Map2.png"), dpi=300, width=7, height=4)
+ggsave(paste("./plots/Map1.png"), dpi=300, width=7, height=4)
 
 
 # Public Support  
@@ -55,7 +43,81 @@ state_choropleth(PolsByState,
                  legend  = "% Support",
                  buckets = 1)
 
-ggsave(paste("./plots/Map3.png"), dpi=300, width=7, height=4)
+ggsave(paste("./plots/Map2.png"), dpi=300, width=7, height=4)
+
+
+
+### put them together 
+
+# Politician Support
+PolsByState$region <- tolower(PolsByState$State)
+PolsByState$value <- PolsByState$PercOfPolsSupp * 100
+
+p1 = state_choropleth(PolsByState,
+                      title   = "Politicians Who Support Gay Marriage",
+                      legend  = "% Support",
+                      buckets = 1)
+
+
+
+# Public Support  
+PollsByState$region <- tolower(PollsByState$state)
+PolsByState$value <- PollsByState$support 
+
+p2 = state_choropleth(PolsByState,
+                      title   = "Public Support for Gay Marriage",
+                      legend  = "% Support",
+                      buckets = 1)
+
+g = arrangeGrob(p1, p2)
+ggsave(paste("./plots/Map3.png"), g, dpi=300, width=7, height=8)
+
+
+# Public Support + No op  
+PollsByState$region <- tolower(PollsByState$state)
+PolsByState$value <- PollsByState$support + PollsByState$X..no.opinion 
+
+state_choropleth(PolsByState,
+                 title   = "Public Support for Gay Marriage",
+                 legend  = "% Support OR no opinion",
+                 buckets = 1)
+
+ggsave(paste("./plots/Map4.png"), dpi=300, width=7, height=4)
+
+
+
+
+# Opposed
+PolsByState$region <- tolower(PolsByState$State)
+PolsByState$value <- PolsByState$PercOfPolsOpp * 100
+
+state_choropleth(PolsByState,
+                 title   = "Politicians Who Oppose Gay Marriage",
+                 legend  = "% Opposed",
+                 buckets = 1)
+
+ggsave(paste("./plots/Map5.png"), dpi=300, width=7, height=4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -77,4 +139,11 @@ choro$legend  = "% Opposed"
 # choro$buckts = 1
 choro$ggplot_scale = scale_fill_brewer(name="% Opposed", palette = "YlGnBu") 
 choro$render()
-ggsave(paste("./plots/Map2.png"), dpi=300, width=8, height=4)
+ggsave(paste("./plots/Map5.png"), dpi=300, width=8, height=4)
+
+
+
+
+
+
+
